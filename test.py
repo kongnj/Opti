a77a1ead2f0ba6c26f9223dfac5050a6aee80cbc
kongnj <54617616+kongnj@users.noreply.github.com>
2024-04-24 09:25:18 +1000

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+"a fffrrr" + """ b
+c """ + """ f """
+
+string = """
+I told everyone in the world: '''
+
+I love my wife
+
+'''
+
+you should understand what I mean
+Yes, it means ''' I'm not sure what I mean '''
+
+
+"""
+
+"""!#$&$(#(#($$**&&###)$(#&@#"""
+
+'''ufu929t9'''
